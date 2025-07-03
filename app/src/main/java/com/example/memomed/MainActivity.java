d0921d01eb2205ce4de17b4dd6c9953540aba7ea
Alessandro Buonaguro <a.buonaguro5@studenti.unisa.it>
2025-07-03 12:09:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/memomed/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.memomed;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true); // se usi JS
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/Login.html");
+
+    }
+}
